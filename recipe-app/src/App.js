1917a19112cb2c6295eeeade7c8a75ec21f12b5d
Shubham Bhardwaj <shubham200015@yahoo.com>
2020-11-09 11:42:34 +0530

    Connect the edamam recipe API with the recipe-app

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React,{useEffect,useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
+const App = () =>{
+
+  const APP_ID = "ec0b9784";
+  const APP_KEY = "82aa623f8ed9c1063bb8b1b29f088412";
+
+  useEffect(()=>{
+    getRecipes();
+  },[]);
+
+  const getRecipes = async ()=>{
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = response.json();
+    console.log(data);
+  }
+  
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">Submit</button>
+      </form>
     </div>
   );
 }
