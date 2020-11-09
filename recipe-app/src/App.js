c7679c56e93763658ccd3d19045449be43d2e7d2
Shubham Bhardwaj <shubham200015@yahoo.com>
2020-11-09 13:07:35 +0530

    Add recipe component to render out the recipes in recipe-a

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,4 +1,5 @@
 import React,{useEffect,useState} from 'react';
+import Recipe from './components/Recipe';
 import './App.css';
 
 const App = () =>{
@@ -6,14 +7,18 @@ const App = () =>{
   const APP_ID = "ec0b9784";
   const APP_KEY = "82aa623f8ed9c1063bb8b1b29f088412";
 
+  const [recipes, setRecipes]=useState([]);
+
   useEffect(()=>{
     getRecipes();
   },[]);
 
   const getRecipes = async ()=>{
-    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
-    const data = response.json();
-    console.log(data);
+    const response = await fetch(`https://api.edamam.com/search?q=chocolate&app_id=${APP_ID}&app_key=${APP_KEY}`);
+
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
   }
   
   return(
@@ -22,6 +27,13 @@ const App = () =>{
         <input className="search-bar" type="text"/>
         <button className="search-button" type="submit">Submit</button>
       </form>
+      {recipes.map(recipe=>(
+        <Recipe
+        key={recipe.recipe.label} 
+        title={recipe.recipe.label} 
+        calories={recipe.recipe.calories} 
+        image={recipe.recipe.image}/>
+      ))}
     </div>
   );
 }
