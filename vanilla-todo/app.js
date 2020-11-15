cd1f589c324c9beb517836112ceb608e9ffdb01b
Shubham Bhardwaj <shubham200015@yahoo.com>
2020-11-15 13:20:50 +0530

    Add basic interactivity of adding todo to todo-app

---
+++ vanilla-todo/app.js
@@ -0,0 +1,38 @@
+// Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+// Event listeners
+todoButton.addEventListener('click',addTodo);
+
+// Functions
+function addTodo(event){
+    // Prevent form from submitting
+    event.preventDefault();
+
+    // Create todo div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+
+    // Create Li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = "hello";
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+
+    // Completed or checkmark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton);
+
+    // Trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('trash-btn');
+    todoDiv.appendChild(trashButton);
+
+    // Append to list
+    todoList.appendChild(todoDiv);
+}
