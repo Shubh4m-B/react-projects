bf51155d8d45067638bb6d0a0325cbad01f3a2f5
Shubham Bhardwaj <shubham200015@yahoo.com>
2020-12-31 14:04:51 +0530

    Add pokecard and pokedex component to pokedex app

---
+++ pokedox/src/Pokecard.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class Pokecard extends Component{
+    render(){
+        return;
+    }
+}
+
+export default Pokecard;
---
+++ pokedox/src/Pokedex.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class Pokedex extends Component{
+    render(){
+        return;
+    }
+}
+
+export default Pokedex;
