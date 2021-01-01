7a3c439cb36b24c644c33735229d7fb0ed7dbf88
Shubham Bhardwaj <shubham200015@yahoo.com>
2021-01-01 12:28:27 +0530

    Make the basics of pokecard in pokedex app

--- pokedox/src/App.js
+++ pokedox/src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React,{Component} from 'react';
+import Pokecard from './Pokecard';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Pokecard id={4} name="Charmander" type="fire" exp={62}/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- pokedox/src/Pokecard.js
+++ pokedox/src/Pokecard.js
@@ -1,8 +1,21 @@
 import React, {Component} from 'react';
+const POKE_API = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 
 class Pokecard extends Component{
     render(){
-        return;
+        let imgSrc = `${POKE_API}${this.props.id}.png`;
+        return (
+            <div className="Pokecard">
+                <h1>{this.props.name}</h1>
+                <img src={imgSrc}/>
+                <p>
+                    <b>Type: </b>{this.props.type}
+                </p>
+                <p>
+                    <b>Base Xp: </b>{this.props.exp}
+                </p>
+            </div>
+        );
     }
 }
 
