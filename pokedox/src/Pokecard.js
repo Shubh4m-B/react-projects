4cbf1957000d3b5421aef540108499810249dd62
Shubham Bhardwaj <shubham200015@yahoo.com>
2021-01-01 13:12:30 +0530

    Add pokedex component in pokedex app

--- pokedox/src/Pokecard.js
+++ pokedox/src/Pokecard.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import './Pokecard.css';
+
 const POKE_API = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 
 class Pokecard extends Component{
