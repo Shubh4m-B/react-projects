60bf415e908b18933afad1a00027420583998550
Shubham Bhardwaj <shubham200015@yahoo.com>
2021-02-24 12:26:51 +0530

    Display the game board in Lights-Out app

--- Lights-Out/src/Board.js
+++ Lights-Out/src/Board.js
@@ -31,10 +31,20 @@ import './Board.css';
 
 class Board extends Component {
 
+  static defaultProps={
+    nrows:5,
+    ncols:5,
+    chanceLightsStartsOn:0.25
+  };
+
   constructor(props) {
     super(props);
 
     // TODO: set initial state
+    this.state={
+      hasWon: false,
+      board: this.createBoard()
+    }
   }
 
   /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
@@ -42,6 +52,13 @@ class Board extends Component {
   createBoard() {
     let board = [];
     // TODO: create array-of-arrays of true/false values
+    for(let y = 0; y < this.props.nrows; y++){
+      let row = [];
+      for(let x = 0; x < this.props.ncols; x++){
+        row.push(Math.random() < this.props.chanceLightsStartsOn)
+      }
+      board.push(row);
+    }
     return board
   }
 
@@ -66,21 +83,27 @@ class Board extends Component {
     // win when every cell is turned off
     // TODO: determine is the game has been won
 
-    this.setState({board, hasWon});
+    // this.setState({board, hasWon});
   }
 
 
   /** Render game board or winning message. */
 
   render() {
-
-    // if the game is won, just show a winning msg & render nothing else
-
-    // TODO
-
-    // make table board
-
-    // TODO
+    let tableBoard = [];
+    for(let y= 0 ; y < this.props.nrows; y++){
+      let row = [];
+      for(let x = 0; x < this.props.ncols; x++){
+        let coord = `${y}-${x}`;
+        row.push(<Cell key={coord} isLit={this.state.board[y][x]}/>);
+      }
+      tableBoard.push(<tr key={y}>{row}</tr>);
+    }
+    return(
+      <table className="Board">
+        <tbody>{tableBoard}</tbody>
+      </table>
+    )
   }
 }
 
